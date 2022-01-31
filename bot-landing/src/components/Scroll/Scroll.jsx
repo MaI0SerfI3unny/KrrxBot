2e6850b06556229e16bd8dc7cb38af2552af43e6
D.edkin <D.edkin.komp.kiev.ua>
2022-01-31 12:08:23 +0200

    seo fix v0.3

--- bot-landing/src/components/Scroll/Scroll.jsx
+++ bot-landing/src/components/Scroll/Scroll.jsx
@@ -20,16 +20,7 @@ const Scroll = () => {
   if (!visible) {return false}
 
   return(
-    <div style={{
-      background:'#009D32',
-      position: 'fixed',
-      padding: 14,
-      bottom: 70,
-      borderRadius: 10,
-      cursor: 'pointer',
-      border: '1px solid white',
-      right: 70}}
-      onClick={() => scrollTop()}>
+    <div className="scrollContainer" onClick={() => scrollTop()}>
       <img style={{width: 30}} src={arrow} alt="scroll" />
     </div>
   )
